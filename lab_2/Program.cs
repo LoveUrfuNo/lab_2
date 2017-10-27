40078a94671fbbd368f5aa103212312c4721fb97
Andrey <andreykostyuchenko87@gmail.com>
2017-10-27 20:42:03 +0500

    plz

---
+++ lab_2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab_2
+{
+    class Program
+    {
+        public const string ADD = "add";
+        public const string SUB = "sub";
+        public const string MULTIPLY = "mul";
+        public const string DIVIDEBY = "div";
+        public const string EXIT = "exit";
+
+        public const int USER_COMMAND_NUMBER = 0;
+        public const int FIRST_RATIONAL_NUMBER = 1;
+        public const int SECOND_RATIONAL_NUMBER = 2;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Привет! Я жду команд!");
+            Console.Write(" >> ");
+
+            ProgramCycle();
+        }
+
+        private static void ProgramCycle()
+        {
+            bool isProgramRunning = true;
+            while (isProgramRunning)
+            {
+                string commandLine = Console.ReadLine();
+                string[] commandLineParts = commandLine.ToLower().Split(' ');
+
+                Rational firstRational = new Rational();
+                Rational secondRational = new Rational();
+                try
+                {
+                    if (!commandLineParts[USER_COMMAND_NUMBER].Equals(EXIT))
+                    {
+                        bool isFirstRationalCorrect = Rational.TryParse(
+                            commandLineParts[FIRST_RATIONAL_NUMBER], out firstRational);
+                        bool isSecondRationalCorrect = Rational.TryParse(
+                            commandLineParts[SECOND_RATIONAL_NUMBER], out secondRational);
+                        if (!(isFirstRationalCorrect || isSecondRationalCorrect))
+                        {
+                            throw new FormatException();
+                        }
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Введите корректное кол-во чисел");
+                    Console.Write(" >> ");
+
+                    continue;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Неверный формат введеных чисел");
+                    Console.Write(" >> ");
+
+                    continue;
+                }
+
+                Rational result = new Rational();
+                switch (commandLineParts[USER_COMMAND_NUMBER])
+                {
+                    case ADD:
+                        result = firstRational.Add(secondRational);
+                        break;
+
+                    case SUB:
+                        result = firstRational.Add(secondRational.Negate());
+                        break;
+
+                    case MULTIPLY:
+                        result = firstRational.Multiply(secondRational);
+                        break;
+
+                    case DIVIDEBY:
+                        result = firstRational.DivideBy(secondRational);
+                        break;
+
+                    case EXIT:
+                        isProgramRunning = false;
+                        Console.WriteLine("Пока");
+                        break;
+
+                    default:
+                        Console.WriteLine("Вы ввели некоректную команду");
+                        break;
+                }
+
+                if (isProgramRunning)
+                {
+                    if (result.Numerator != 0 && result.Denominator != 0)
+                    {
+                        Console.WriteLine(result);
+                    }
+                    Console.Write(" >> ");
+                }
+            }
+        }
+    }
+}
---
+++ lab_2/Rational.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2
+{
+    public struct Rational
+    {
+        /// Числитель дроби
+        public int Numerator { get; set; }
+
+        /// Знаменатель дроби
+        public int Denominator { get; set; }
+
+        /// Целая часть числа Z.N:D, Z. получается делением числителя на знаменатель и
+        /// отбрасыванием остатка
+        public int Base => (int)Math.Truncate((double)Numerator / Denominator);
+
+        /// Дробная часть числа Z.N:D, N:D
+        public double Fraction => Numerator / Denominator - Base;
+
+        /// Операция сложения, возвращает новый объект - рациональное число,
+        /// которое является суммой чисел c и this
+        public Rational Add(Rational c)
+        {
+            Rational rational = new Rational();
+            rational.Numerator = c.Numerator * this.Denominator + this.Numerator * c.Numerator;
+            rational.Denominator = c.Denominator * this.Denominator;
+
+            Even();
+
+            return rational;
+        }
+
+        /// Операция смены знака, возвращает новый объект - рациональное число,
+        /// которое являтеся разностью числа 0 и this
+        public Rational Negate()
+        {
+            Rational rational = new Rational();
+            rational.Denominator = this.Denominator;
+            rational.Numerator = -this.Numerator;
+
+            Even();
+
+            return rational;
+        }
+
+        /// Операция умножения, возвращает новый объект - рациональное число,
+        /// которое является результатом умножения чисел x и this
+        public Rational Multiply(Rational x)
+        {
+            Rational rational = new Rational();
+            rational.Numerator = x.Numerator * this.Numerator;
+            rational.Denominator = x.Denominator * this.Denominator;
+
+            Even();
+
+            return rational;
+        }
+
+        /// Операция деления, возвращает новый объект - рациональное число,
+        /// которое является результатом деления this на x
+        public Rational DivideBy(Rational x)
+        {
+            Rational rational = new Rational();
+            rational.Numerator = this.Numerator * x.Denominator;
+            rational.Denominator = this.Denominator * x.Numerator;
+
+            return rational;
+        }
+
+        /// Вовзращает строковое представление числа в виде Z.N:D, где
+        /// Z — целая часть N и D — целые числа, числитель и знаменатель, N < D
+        /// '.' — символ, отличающий целую часть от дробной,
+        /// ':' — символ, обозначающий знак деления
+        /// если числитель нацело делится на знаменатель, то
+        /// строковое представление не отличается от целого числа
+        /// (исчезает точка и всё, что после точки)
+        /// Если Z = 0, опускается часть представления до точки включительно
+        public override string ToString()
+        {
+            int numerator = Numerator;
+            while (numerator >= Denominator)
+            {
+                numerator -= Denominator;
+            }
+
+            string fraction = (numerator == 0 ? "" : "." + numerator + ":" + Denominator);
+
+            return Base + fraction;
+        }
+
+        /// Создание экземпляра рационального числа из строкового представления Z.N:D
+        /// допускается N > D, также допускается
+        /// Строковое представления рационального числа
+        /// Результат конвертации строкового представления в рациональное
+        /// число
+        /// true, если конвертация из строки в число была успешной,
+        /// false если строка не соответствует формату
+        public static bool TryParse(string input, out Rational result)
+        {
+            result = new Rational();
+
+            if (input.Contains('-'))
+            {
+                Console.WriteLine("Can't parse negative value");
+
+                return false;
+            }
+
+            string[] numberParts = input.Split('.', ':');
+            try
+            {
+                if (input.Contains('.'))
+                {
+                    result.Denominator = int.Parse(numberParts[2]);
+                    result.Numerator =
+                        int.Parse(numberParts[0]) * result.Denominator + int.Parse(numberParts[1]);
+                }
+                else
+                {
+                    result.Denominator = int.Parse(numberParts[1]);
+                    result.Numerator = int.Parse(numberParts[0]);
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("TryParse exception -> " + e.Message);
+
+                return false;
+            }
+        }
+
+        /// Приведение дроби - сокращаем дробь на общие делители числителя
+        /// и знаменателя. Вызывается реализацией после каждой арифметической операции
+        private void Even()
+        {
+            if (Numerator == 0 || Denominator == 0)
+            {
+                return;
+            }
+
+            int nod = getNod(Numerator, Denominator);
+            Numerator = Numerator / nod;
+            Denominator = Denominator / nod;
+        }
+
+        private int getNod(int x, int y)
+        {
+            return y == 0 ? x : getNod(y, x % y);
+        }
+    }
+}
